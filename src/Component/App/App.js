63bf743e991057bdf236ef538a36e19659fcc738
Thy696 <thyvu411@gmail.com>
2020-05-13 13:29:25 -0500

    problem

--- src/Component/App/App.js
+++ src/Component/App/App.js
@@ -8,12 +8,23 @@ class App extends React.Component {
     console.log(this.props)
   }
 
+  handleClick = () =>{
+    console.log('in handleClick');
+    this.props.dispatch({
+      type: 'test00', 
+      payload: 'tests info'
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <p>Lamport Combine Logger</p>
         </header>
+        <button onClick = {this.handleClick}>Test</button>
+        <p>Reducer 1:{this.props.reduxState.reducerOne}</p>
+        <p>Reducer 2:{this.props.reduxState.reducerTwo}</p>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,19 +4,53 @@ import './index.css';
 import App from './Component/App/App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore , combineReducers, applyMiddleware} from 'redux';
+import logger from 'redux-logger';
+
+import {takeEvery, put} from 'redux-saga/effects' // put is the name, takeEvery is listen
+import createSagaMiddleware from 'redux-saga';
 
 const reducerOne = (state= 0 , action) => {
-  console.log('in reducerOne: ', action);
+  //console.log('in reducerOne: ', action);
   return state;
 }
 
-const store = createStore(reducerOne);
+const reducerTwo = (state= "sdfsdf" , action) => {
+  //console.log('in reducerTwo: ', action);
+  if(action.type === 'test'){
+    console.log('in reducer 2');
+    state = 'tester'
+  }
+  return state;
+}
+
+
+//generator function
+function*watcherSaga(){
+//watcher is going to listen for Saga things
+//generator function so we can do asynx stuff
+//we will ues "yield" in these
+yield takeEvery('test00', testSaga);
+}//end watcher
+
+function*testSaga(action){
+  console.log('in testSaga',action);
+}
+
+const sagaMiddleware = createSagaMiddleware( watcherSaga );
+
+
+
+const store = createStore(
+  combineReducers({reducerOne,reducerTwo}),
+  applyMiddleware(logger, sagaMiddleware) // second argument
+  );
+
 
 ReactDOM.render(
   <Provider store = {store}>
     <App />
-  </Provider>  , 
+  </Provider>, 
   document.getElementById('root')
 );
 
